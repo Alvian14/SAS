9880b4d2e03ac8e442c6bef99b45c771b32bc36c
Alvian14 <cidukgaming03@gmail.com>
2025-09-23 11:53:48 +0700

    add kelas.kelas

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -91,7 +91,7 @@
           </li>
           <span class="divider"><hr /></span>
           <li class="nav-item">
-                <a href="kelas.html">
+                <a href="{{ route('kelas.kelas') }}">
                     <span class="icon">
                         <!-- Ikon kelas / papan tulis -->
                         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
