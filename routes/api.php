79c144d604cdff50faeb32f2c460727c6eb7c3a5
Pramudya <pramudyap333@gmail.com>
2026-02-28 20:39:35 +0700

    feat: update api routes notes

--- routes/api.php
+++ routes/api.php
@@ -10,16 +10,21 @@ Route::get('/ping', function () {
     return response()->json(['message' => 'pong'], 200);
 });
 
+// auth routes
 Route::post('/login', [UserController::class, 'login']);
-Route::middleware('auth:sanctum')->post(
-    '/store-fcm',
-    [UserController::class, 'storeFcmToken']
-);
 Route::post('/register', [UserController::class, 'register']);
+
+// global route for public access
 Route::get('/classes', [ClassController::class, 'index']);
 // get schedules for a specific class (only active academic period schedules)
 Route::get('/classes/{id}/schedule', [ClassController::class, 'schedule']);
 Route::get('/classes/{id}/schedule/{dayindex}', [ClassController::class, 'scheduleByDay']);
+
+// protected routes, need authentication
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/attendance-daily/report', [AttendanceController::class, 'dailyAttendanceReport']);
+    Route::post('/store-fcm', [UserController::class, 'storeFcmToken']);
+});
 
 // route login pakai Sanctum
 Route::middleware(['auth:sanctum', 'role:student'])->group(function () {
