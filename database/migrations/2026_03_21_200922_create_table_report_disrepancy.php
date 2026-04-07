768f8812c1beee57fab84d9cd92c8aaad58640db
Pramudya <pramudyap333@gmail.com>
2026-04-07 20:47:58 +0700

    feat: update table report disrepancy

--- database/migrations/2026_03_21_200922_create_table_report_disrepancy.php
+++ database/migrations/2026_03_21_200922_create_table_report_disrepancy.php
@@ -14,12 +14,14 @@ return new class extends Migration
         Schema::create('table_report_disrepancy', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_student')->constrained('students')->onDelete('cascade');
+            $table->string('student_name');
             $table->foreignId('reported_by')->constrained('users')->onDelete('cascade'); // admin or teacher who report the discrepancy
             $table->foreignId('id_attendance_history')->constrained('attendance_histories')->onDelete('cascade'); // for specific attendance record that has discrepancy
-            $table->foreignId('id_schedule')->constrained('schedules')->onDelete('cascade');
+            // $table->foreignId('id_schedule')->constrained('schedules')->onDelete('cascade');
             $table->foreignId('id_class')->constrained('classes')->onDelete('cascade');
-            $table->enum('discrepancy_type', ['terlambat', 'hp_tidak_tersedia', 'izin', 'pulang_awal', 'alasan_lain'])->nullable(); // jenis ketidaksesuaian
+            $table->enum('disrepancy_type', ['terlambat', 'hp_tidak_tersedia', 'izin', 'pulang_awal', 'alasan_lain'])->nullable(); // jenis ketidaksesuaian
             $table->text('description')->nullable();
+            $table->boolean('marked_as_resolved')->default(false);
             $table->date('attendance_date');
             $table->timestamps();
         });
